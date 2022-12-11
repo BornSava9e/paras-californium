983131da43fb2eeb52ca28541d40c9a89b863308
BornSava9e <Parasanand1999@gmail.com>
2022-12-11 21:15:59 +0530

    assignmentgetapi

---
+++ get-api/movies.js
@@ -0,0 +1,4 @@
+let movies =  ["rang de basanti","The shining","Lord of the rings", "batman begins"]
+function mvrtr(num){
+    console.log(movies[0])
+}
--- src/routes/route.js
+++ src/routes/route.js
@@ -17,40 +17,85 @@ router.get('/test-me', function (req, res) {
     let result = _.first(days, 4)
     console.log(`Result from underscore function is ${result}`)
 
-    logger1.welcome("Paras");
-    util1.printDate();
-    util1.printMonth();
-    util1.getBatchInfo();
-    console.log(validator.clearWhiteSpace("                 Hello                    "))
-    console.log(validator.lwrCase("FunCTioNuP"));
-    console.log(validator.uprCase("FunctionUp"));
+    // logger1.welcome("Paras");
+    // util1.printDate();
+    // util1.printMonth();
+    // util1.getBatchInfo();
+    // console.log(validator.clearWhiteSpace("                 Hello                    "))
+    // console.log(validator.lwrCase("FunCTioNuP"));
+    // console.log(validator.uprCase("FunctionUp"));
     
-    let month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    console.log(load.chunk(month, 4))
+    // let month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    // console.log(load.chunk(month, 4))
 
-    let num = [1,3,5,7,9,11,13,15,17,19]
-    console.log(load.tail(num))
+    // let num = [1,3,5,7,9,11,13,15,17,19]
+    // console.log(load.tail(num))
 
-    let arr1 = [1,2,3]
-    let arr2 = [4,2,5]
-    let arr3 = [7,6,3]
-    let arr4 = [1,8,9]
-    let arr5 = [1,2,10]
+    // let arr1 = [1,2,3]
+    // let arr2 = [4,2,5]
+    // let arr3 = [7,6,3]
+    // let arr4 = [1,8,9]
+    // let arr5 = [1,2,10]
 
-    console.log(load.union(arr1,arr2,arr3,arr4,arr5))
+    // console.log(load.union(arr1,arr2,arr3,arr4,arr5))
 
-    const array1 = [ ["horror","The Shinning"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labrinth"] ]
-    let result1 = load.fromPairs(array1)
-    console.log(result1)
+    // const array1 = [ ["horror","The Shinning"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labrinth"] ]
+    // let result1 = load.fromPairs(array1)
+    // console.log(result1)
 
-    res.send('any dummy text')
+    const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+
+    res.send(movies)
 });
+
+// 2 question
+
+router.get('/movies/:indexNumber', function(req, res){
+    const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    let a = movies[req.params.indexNumber]
+    res.send(a)
+})
+ //3 question
+router.get('/movie/:indexNum1', function(req, res){
+    const movies = ["Top gun Maverick","Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    let a = req.params.indexNum1
+    let javi = movies[a]
+    if(a<=movies.length-1){
+        return res.send(javi)
+    }else{
+    res.send("please use a valid number")
+    }
+})
+//4 question
+router.get('/films', function(req, res){
+    let movie = [{id: 1, name:"The shining"},
+    {id: 2, name:"Incendies"},
+    {id: 3, name:"Rang de Basanti"},
+    {id: 4, name:"Finding Nemo"}
+    ]
+    res.send(movie)
+})
+//5 question
+router.get('/films/:filmId', function(req, res){
+   let i = req.params.filmId    
+        let arrr = [{id: 1, name:"The shining"},
+        {id: 2, name:"Incendies"},
+        {id: 3, name:"Rang de Basanti"},
+        {id: 4, name:"Finding Nemo"}
+        ]
+
+        for(let j = 0; j<arrr.length; j++){
+            if(i==arrr[j].id){
+                return res.send(arrr[j])
+            }
+        }
+    res.send("No movie exists with this id")
+})
 
 
 router.get('/test-you', function(req, res){
     console.log("I am here")
     res.send("very important text")
 })
-
 
 module.exports = router;
