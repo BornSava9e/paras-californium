f5907aa1a91d2de2822f62064ca9eb8138a49f86
BornSava9e <Parasanand1999@gmail.com>
2022-12-14 14:33:22 +0530

    postapi

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,46 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+
+let players =[       {
+    "name": "manish",
+    "dob": "1/1/1995",
+    "gender": "male",
+    "city": "jalandhar",
+    "sports": [
+        "swimming"
+    ]
+},
+{
+    "name": "gopal",
+    "dob": "1/09/1995",
+    "gender": "male",
+    "city": "delhi",
+    "sports": [
+        "soccer"
+    ]
+},
+{
+    "name": "lokesh",
+    "dob": "1/1/1990",
+    "gender": "male",
+    "city": "mumbai",
+    "sports": [
+        "soccer"
+    ]
+},
+]
+
+       router.post('/players', function(req,res){
+        const person =  req.body
+        let playerInfo = players.find(player => player.name === person.name )
+        if(playerInfo){
+            return res.send('player already exists')
+        }else{
+            players.push(person)
+            return res.send(players)
+        }
+       })
+
+
 module.exports = router;
