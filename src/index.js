80e408a3d8ccceb4caa531c31e45067f2c1352f1
BornSava9e <Parasanand1999@gmail.com>
2022-12-16 08:54:53 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const app = express();
+const mongoose = require("mongoose")
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', route);
+
+mongoose.connect("mongodb+srv://Paras_Anand:paras4321@cluster0.3z8igom.mongodb.net/Paras_db",
+{usenewUrlparser:true})
+.then(()=>{console.log("mongodb is connected")})
+.catch(err=> console.log(err))
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
